656d5362694cd25b001a724cd18eff53b1480952
ulumdev <belajarkoding97@gmail.com>
2025-06-07 18:22:11 +0700

    Latihan Studi Kasus: Memberikan Styling

--- src/components/ContactsApp.jsx
+++ src/components/ContactsApp.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ContactList from "./ContactList";
-import {getData} from "./data";
+import {getData} from "../utils/data";
 
 function ContactsApp() {
     const contacts = getData();
 
     return (
-        <div className="contacts-app">
-            <h1 className="contacts-app__title">Contacts</h1>
+        <div className="contact-app">
+            <h1 className="contact-app__title">Contacts</h1>
             <ContactList contacts={contacts} />
         </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import ContactsApp from "./ContactsApp";
+import ContactsApp from "./components/ContactsApp";
+
+import "./styles/style.css"; // Assuming you have a styles.css file for styling
 
 const root = createRoot(document.getElementById("root"));
 root.render(<ContactsApp />);
